36ea4e0d8b4f3d69f41b25ab36f84251120fe1e1
Andrii <andrew.voytovich@boost.solutions>
2018-03-23 14:23:42 +0200

    diveded two processes: hosting server and set webhook on bot

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 const http = require('http');
-const { getPublicUrl } = require('./server/helper/function');
 const bot = require('./server/helper/bot');
 const { message } = require('./server/helper/message');
 
-getPublicUrl()
-  .then((url) => {
-    const port = parseInt(process.env.PORT, 10) || 5000;
-    bot.setWebhook(url);
-    http.createServer(bot.middleware()).listen(port, () => console.log(message.server_running + port));
-  })
-    .catch((err) => console.log('err', err));
+const port = parseInt(process.env.PORT, 10) || 5000;
+http.createServer(bot.middleware()).listen(port, () => console.log(message.server_running + port));
--- server/helper/bot.js
+++ server/helper/bot.js
@@ -1,5 +1,5 @@
 const ViberBot = require('viber-bot').Bot;
-const { checkUrlAvailability } = require('./function');
+const { checkUrlAvailability, getPublicUrl } = require('./function');
 const { secret } = require('./../../config/config.env');
 const logger = require('./logger');
 
@@ -8,5 +8,9 @@ const bot = new ViberBot(logger, secret);
 bot.onTextMessage(/./, (message, response) => {
   checkUrlAvailability(response, message.text);
 });
+
+getPublicUrl()
+  .then((url) => bot.setWebhook(url))
+    .catch((err) => console.log('err', err));
 
 module.exports = bot;
